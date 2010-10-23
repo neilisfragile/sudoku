de70bb14e974bb463831bf0b1b0301dd4270dcc4
neil johnson <neil@fragile.org.uk>
2010-10-23 19:03:04 +0100

    remove prints and fix comment typo

--- src/main/sudoku/strategy/FirstAvailable.java
+++ src/main/sudoku/strategy/FirstAvailable.java
@@ -1,9 +1,7 @@
 package sudoku.strategy;
 
-import java.util.Stack;
 
 import sudoku.Board;
-import sudoku.SquareCandidate;
 /**
  * The 'FirstAvailable' strategy always selects the first valid choice
  * it finds in any situation
@@ -11,25 +9,22 @@ import sudoku.SquareCandidate;
  *
  */
 public class FirstAvailable implements Strategy {
-    Stack<SquareCandidate> suggestionStack = new Stack<SquareCandidate>();
     public void solve(Board board) {
     	long startTime= System.nanoTime();
         final int[] initialState = board.retrieveState().clone();
         int[] workingState = board.retrieveState();
         //loop through all
         //find lowest number that is sound, 
-        //if find a sqaure where no sound exists
+        //if find a square where no sound option exists
         //roll back to previous initial start as 0
         //find next sound and proceed.
         for(int i=0;i<initialState.length;i++){
-            //System.out.println("i is "+i);
             boolean updatedBoard = false;
             if (initialState[i]==0){
                 for(int j=workingState[i]+1;j<=9;j++){
                     if(board.isSound(i,j)) {
                         workingState[i] = j;
                         updatedBoard=true;
-                        //System.out.println("Setting "+i+" to "+j);
                         break;
                     }
                 }
@@ -42,10 +37,7 @@ public class FirstAvailable implements Strategy {
                     while (initialState[i-k]!=0){
                         k++;
                         
-                    }
-                    //System.out.print("i was "+i);
-                    i-=(k+1);
-                    //System.out.println(" rolling i back to "+i);
+                    }i-=(k+1);
                 }
             }   
         }
